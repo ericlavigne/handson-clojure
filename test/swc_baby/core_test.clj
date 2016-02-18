c54675ef00cae64df526520c0168ce257fae827b
Imre Koszo <imrekoszo@users.noreply.github.com>
2016-02-18 20:34:41 +0000

    refactor

--- test/swc_baby/core_test.clj
+++ test/swc_baby/core_test.clj
@@ -2,17 +2,24 @@
   (:require [clojure.test :refer :all]
             [swc-baby.core :refer :all]))
 
-(def d [[5 "V"]
+(def d [[10 "X"]
+        [9 "IX"]
+        [5 "V"]
         [4 "IV"]])
 
 (defn roman [x]
-  (if-let [entry (first (filter (fn [e] (= x (first e))) d))]
-    (second entry)
-    (apply str (take x (repeat "I")))))
+  (let [[d r :as entry] (first (filter (fn [e] (>= x (first e))) d))
+        ]
+    (if entry
+      r
+      (apply str (take x (repeat "I"))))))
 
 (deftest can-convert-decimal-to-roman
   (is (= "" (roman 0)))
   (is (= "I" (roman 1)))
   (is (= "II" (roman 2)))
   (is (= "IV" (roman 4)))
-  (is (= "V" (roman 5))))
+  (is (= "V" (roman 5)))
+  (is (= "IX" (roman 9)))
+  (is (= "X" (roman 10)))
+  )
