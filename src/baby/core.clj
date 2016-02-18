7a2b904449ee3f2ba3ef531db841b98a32847a63
Eric Lavigne <elavigne@mcna.net>
2016-02-18 14:17:13 -0500

    prepare to use find-lowest-key

--- src/baby/core.clj
+++ src/baby/core.clj
@@ -13,6 +13,7 @@
   (apply max (filter #(<= % dec) (keys conversion-table))))
 
 (defn dec-to-roman [dec]
-  (or (conversion-table dec)
-    "II"))
+  (let [lowest (find-lowest-key dec)]
+    (or (conversion-table dec)
+      "II")))
 
