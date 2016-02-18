dba89b2886ff816b8c5e99f630e6e8a6d7131304
Eric Lavigne <elavigne@mcna.net>
2016-02-18 14:18:18 -0500

    base case for recursion

--- src/baby/core.clj
+++ src/baby/core.clj
@@ -13,7 +13,9 @@
   (apply max (filter #(<= % dec) (keys conversion-table))))
 
 (defn dec-to-roman [dec]
-  (let [lowest (find-lowest-key dec)]
-    (or (conversion-table dec)
-      "II")))
+  (if (<= dec 0)
+    ""
+    (let [lowest (find-lowest-key dec)]
+      (or (conversion-table dec)
+        "II"))))
 
