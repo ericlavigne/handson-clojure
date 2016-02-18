0ccdc427d73c0ef34b086e1cb8c6e6f78b3cf8f2
Imre Koszo <imrekoszo@users.noreply.github.com>
2016-02-18 19:31:04 +0000

    6

--- test/swc_baby/core_test.clj
+++ test/swc_baby/core_test.clj
@@ -3,10 +3,12 @@
             [swc-baby.core :refer :all]))
 
 (defn roman [x]
-  (case x
-    5 "V"
-    4 "IV"
-    (apply str (take x (repeat "I")))))
+  (if (or (= x 5)
+          (> x 5))
+    (str "V" (roman (- x 5)))
+    (if (= x 4)
+      "IV"
+      (apply str (take x (repeat "I"))))))
 
 (deftest can-convert-decimal-to-roman
   (is (= "" (roman 0)))
