b37b9d55dfcc2ecd8ef0b269cc1dc6e84a26d651
Eric Lavigne <elavigne@mcna.net>
2016-02-18 14:16:07 -0500

    find-lowest-key

--- src/baby/core.clj
+++ src/baby/core.clj
@@ -9,6 +9,9 @@
   {1 "I"
    4 "IV"})
 
+(defn find-lowest-key [dec]
+  (apply max (filter #(<= % dec) (keys conversion-table))))
+
 (defn dec-to-roman [dec]
   (or (conversion-table dec)
     "II"))
