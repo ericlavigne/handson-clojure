df0ddf72305d9bd4a575ea589ca90361dce77acd
Imre Koszo <imrekoszo@users.noreply.github.com>
2016-02-18 20:10:01 +0000

    1

--- test/swc_baby/core_test.clj
+++ test/swc_baby/core_test.clj
@@ -3,8 +3,8 @@
             [swc-baby.core :refer :all]))
 
 (defn roman [x]
-  "")
+  (apply str (take x (repeat "I"))))
 
 (deftest can-convert-decimal-to-roman
-  (is (= ""
-         (roman 0))))
+  (is (= "" (roman 0)))
+  (is (= "I" (roman 1))))
