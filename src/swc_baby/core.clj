bbb6b06cd82e71ab42e9219f50588fadeaab6875
Imre Koszo <imrekoszo@users.noreply.github.com>
2016-02-18 18:39:49 +0000

    init

---
+++ src/swc_baby/core.clj
@@ -0,0 +1,6 @@
+(ns swc-baby.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/swc_baby/core_test.clj
@@ -0,0 +1,7 @@
+(ns swc-baby.core-test
+  (:require [clojure.test :refer :all]
+            [swc-baby.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
