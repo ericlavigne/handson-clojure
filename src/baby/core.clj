3dabb9d0f514ec03944871da213fcd8d7c8691ac
Eric Lavigne <elavigne@mcna.net>
2016-02-18 15:44:49 -0500

    delete unused code

--- src/baby/core.clj
+++ src/baby/core.clj
@@ -1,10 +1,5 @@
 (ns baby.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (def conversion-table
   {1 "I"
    4 "IV"
--- test/baby/core_test.clj
+++ test/baby/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [baby.core :refer :all]))
 
-(deftest a-test
-  (testing "Adding numbers"
-    (is (= 1 1))))
-
 (deftest simple-conversions
   (testing "convert some numbers"
     (is (= "I" (dec-to-roman 1)))
